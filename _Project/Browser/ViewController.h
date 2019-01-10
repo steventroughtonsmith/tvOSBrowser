6590c529cfa199d995842eb29ea7951532328895
jvanakker <jipvanakker@Jips-MacBook-Pro.local>
2019-01-10 15:50:45 +0100

    New top Navigation bar added

--- _Project/Browser/ViewController.h
+++ _Project/Browser/ViewController.h
@@ -10,6 +10,23 @@
 #import <GameKit/GameKit.h>
 
 @interface ViewController : GCEventViewController
+{
+    CGFloat topMenuBrowserOffset;
+    Boolean topMenuShowing;
+}
+
+@property (nonatomic, retain) IBOutlet UIVisualEffectView *topMenuView;
+@property (nonatomic, retain) IBOutlet UIView *browserContainerView;
+
+@property (nonatomic, retain) IBOutlet UIImageView *btnImageBack;
+@property (nonatomic, retain) IBOutlet UIImageView *btnImageForward;
+@property (nonatomic, retain) IBOutlet UIImageView *btnImageRefresh;
+@property (nonatomic, retain) IBOutlet UIImageView *btnImageHome;
+@property (nonatomic, retain) IBOutlet UIImageView *btnImageFullScreen;
+@property (nonatomic, retain) IBOutlet UIImageView *btnImgMenu;
+@property (nonatomic, retain) IBOutlet UILabel     *lblUrlBar;
+
+@property (nonatomic, retain) IBOutlet UIActivityIndicatorView *loadingSpinner;
 
 
 @end
